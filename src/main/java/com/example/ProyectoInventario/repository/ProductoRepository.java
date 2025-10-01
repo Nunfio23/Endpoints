dbd3c896cce42d74daf3d0fddcca5f6264cd0f16
Nunfio23 <carloshermadez12@gmail.com>
2025-09-30 22:31:33 -0600

    Resolucion de repository

--- src/main/java/com/example/ProyectoInventario/repository/ProductoRepository.java
+++ src/main/java/com/example/ProyectoInventario/repository/ProductoRepository.java
@@ -1,5 +1,7 @@
 package com.example.ProyectoInventario.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,8 @@ import com.example.ProyectoInventario.entity.Producto;
 
 @Repository
 public interface ProductoRepository extends JpaRepository<Producto, Long> {
-    Producto findBySku(String sku);
+
+    // Buscar producto por nombre
+    Optional<Producto> findByNombre(String nombre);
+
 }
