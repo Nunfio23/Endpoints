39b6bcdf7d05ab3351ac06d298c85f819e3fe700
Nunfio23 <carloshermadez12@gmail.com>
2025-09-30 22:57:24 -0600

    Fix: add import java.util.List to ProductoRepository

--- src/main/java/com/example/ProyectoInventario/repository/ProductoRepository.java
+++ src/main/java/com/example/ProyectoInventario/repository/ProductoRepository.java
@@ -1,16 +1,14 @@
 package com.example.ProyectoInventario.repository;
 
+import com.example.ProyectoInventario.entity.Producto;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;     
 import java.util.Optional;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import com.example.ProyectoInventario.entity.Producto;
-
-@Repository
 public interface ProductoRepository extends JpaRepository<Producto, Long> {
-
-    // Buscar producto por nombre
     Optional<Producto> findByNombre(String nombre);
+
+
     List<Producto> findByCategoriaId(Long categoriaId);
 }
